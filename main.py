40b4ef3b9456732d27429c980c68f38b388a6757
Brandon <brandonwuedu@gmail.com>
2025-03-29 23:07:39 -0700

    setup for us states game

---
+++ main.py
@@ -0,0 +1,22 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(answer_state)
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
+screen.exitonclick()
