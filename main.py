29ca05b46ee60b5be0e38cee7994c6b9c7fa3e96
Brandon <brandonwuedu@gmail.com>
2025-03-31 23:02:11 -0700

    +1 to num guessed and + state name to states guessed when a state from 50_states.csv is guessed correctly

--- main.py
+++ main.py
@@ -7,8 +7,29 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+states_guessed = []
+num_guessed = 0
+
+data = pandas.read_csv("50_states.csv")
+states = data["state"]
+# print(states)
+
+
+while num_guessed != 50:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+    if answer_state in data["state"].values:
+        states_guessed.append(answer_state)
+        num_guessed += 1
+
+        # state = Turtle()
+        # state.goto(50,50)
+        
+        # state.write(f"{answer_state}", True, "center")
+        
+    
+    print(answer_state)
+    print(states_guessed)
+    print(num_guessed)
 
 
 
