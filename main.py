38fa16863b57975591a2808f227852bd265a21b9
Brandon <brandonwuedu@gmail.com>
2025-04-10 23:05:52 -0700

    add title case to work no matter if lower/capital case, add 'exit' option to leave game

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ map = Turtle()
 map.shape(image)
 
 states_guessed = []
-num_guessed = 0
+score = 0
 
 data = pandas.read_csv("50_states.csv")
 
@@ -18,36 +18,30 @@ data = pandas.read_csv("50_states.csv")
 # print(states)
 
 
-while num_guessed != 50:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-    if answer_state in data["state"].values:
+while score != 50:
+    answer_state = screen.textinput(title="Guess the State", prompt=f"{score}/50 States Guessed")
+    answer_state = answer_state.title() # guess works if lowercase, all uppercase, etc.
+
+    if answer_state == "Exit":
+        break
+
+    if answer_state in data["state"].values: # check if guess is among 50 states in csv
         x_coord = data[data["state"] == answer_state]["x"].values[0]
         y_coord = data[data["state"] == answer_state]["y"].values[0]
         # print(x_coord)
         # print(y_coord)
 
         states_guessed.append(answer_state)
-        num_guessed += 1
+        score += 1
 
         state = Turtle()
         state.hideturtle()
         state.pu()
-        state.goto(x_coord, y_coord)
+        state.goto(x_coord, y_coord) # write correct guess onto map at respective x and y coords
         
         state.write(f"{answer_state}", True, "center")
         
     
     print(answer_state)
     print(states_guessed)
-    print(num_guessed)
-
-
-
-
-
-
-
-
-
-map.mainloop()
-screen.exitonclick()
+    print(score)
